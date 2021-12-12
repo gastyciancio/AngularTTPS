7bd31cdd2e743239e3b1ea60b0f888b5ed6be59f
Ciancio, Gaston Ezequiel <gastonciancio4@gmail.com>
2021-12-12 19:37:45 -0300

    cosas mejores ordenadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NuevoServicioComponent } from './servicios/servicio/newServicio/nuevo-servicio/nuevo-servicio.component';
 import { ServicioComponent } from './servicios/servicio/servicio.component';
 
 const routes: Routes = [
  
   { path: 'servicios', component: ServicioComponent,pathMatch: 'full'},
+  { path: 'nuevoServicio', component: NuevoServicioComponent,pathMatch: 'full'},
 
   
        
